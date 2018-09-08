4d25eda2fbf65896bb328f60567f2d9ec3f57570
demian sims <demiansims@gmail.com>
2018-09-08 20:53:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+
+function getFirstSelector(selector) {
+ var first = document.querySelector(selector);
+ return first 
+}
+
+function nestedTarget() {
+  var target = document.querySelector("div.target");
+  return target
+}
+
+function increaseRankBy(n) {
+ 
+  const rankedList = document.querySelectorAll(".ranked-list");
+  for (let i=0; i<rankedList.length; i++) {
+  var finalRank = parseInt(rankedList[i] + n) 
+  
+  rankedList[i].innerHTML = finalRank }
+  
+}  
+
+
+   
+
+
+function deepestChild() {
+
+  var target = document.getElementbyId('grand-node').querySelectorAll('div');
+  for (let i = 0; i < target.length; i++) {
+    
+    var deepChild = target.children[i];
+    }
+    return deepChild
+  }
+  
+  
+
+
+
