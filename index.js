d55579efdfc79b4ccfcc081e4b30797d1f46c650
demian sims <demiansims@gmail.com>
2018-09-21 13:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
   var element = document.querySelector(selector); 
   return element
 }
+
+function nestedTarget() {
+  var target = document.querySelector('#nested .target');
+  return target
+}
