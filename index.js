f4a16c6fbac160be717d4d1de1b4a7a464b9494f
demian sims <demiansims@gmail.com>
2018-09-21 14:48:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n) {
   
 }
 function deepestChild() {
- var deepChild = document.getElementById('grand-node').querySelector('div')
+ var deepChild = document.getElementById('grand-node').querySelectorAll('div')
  var arrayDeep = [];
  for (let i=0; i < deepChild.length; i++) {
    arrayDeep.push(deepChild[i].innerText) 
